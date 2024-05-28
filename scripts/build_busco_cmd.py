81481b259112f6867bd001f89f913715f48c4b19
Clara Pitzschel <s-clpitz@login-a.maas>
2024-05-28 12:25:49 +0000

    added braker or annot file for busco

--- scripts/build_busco_cmd.py
+++ scripts/build_busco_cmd.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Build busco command')
 parser.add_argument('-c', '--csv', type=str, required=True, help='csv file with all info about all species')
 parser.add_argument('-s', '--species', type=str, required=True, help='Species name in data structure')
 parser.add_argument('-o', '--output', type=str, required=True, help='Output file name with annotation command')
-parser.add_argument('-b', '--braker', type=str, required=True, help='Braker file to be analyze')
+#parser.add_argument('-b', '--braker', type=str, required=True, help='Braker file to be analyze')
 parser.add_argument('-t', '--threads', type=int, default=1, help='Number of threads to use')
 parser.add_argument('-w', '--workingdir', type=str, default='.', help='Working directory')
 parser.add_argument('-l', '--logfile', type=str, default='braker.log', help='Log file name')
@@ -19,22 +19,44 @@ df = pd.read_csv(args.csv)
 
 # find the row where column "species" has the value of args.species
 row = df.loc[df['species'] == args.species]
-cmd = ""
-cmd += 'busco --in=' + str(args.braker)
-cmd += ' --mode=proteins'
-cmd += ' --lineage_dataset=' + row['busco_lineage'].values[0]
-cmd += ' --cpu=' + str(args.threads)
-cmd += ' --out=/data/species/' + str(args.species) + '/busco'
-cmd += ' &> ' + args.logfile
+
+cmd_g = ""
+cmd_g += 'busco --in=data/species/' + str(args.species) + '/genome/genome.fa'  
+cmd_g += ' --mode=genome'
+cmd_g += ' --lineage_dataset=' + row['busco_lineage'].values[0]
+cmd_g += ' --cpu=' + str(args.threads)
+cmd_g += ' --out=/data/species/' + str(args.species) + '/busco/genomefile'
+cmd_g += ' &> ' + args.logfile
+
+cmd_b = ""
+cmd_a = ""
+
+if pd.isna(row['annotation_file'].values[0]):
+    cmd_b += 'busco --in=data/species/' + str(args.species) + '/braker/braker.aa'
+    cmd_b += ' --mode=proteins'
+    cmd_b += ' --lineage_dataset=' + row['busco_lineage'].values[0]
+    cmd_b += ' --cpu=' + str(args.threads)
+    cmd_b += ' --out=/data/species/' + str(args.species) + '/busco/brakerfile'
+    cmd_b += ' &> ' + args.logfile
+else:
+    cmd_a += 'busco --in=data/species/' + str(args.species) + '/prot/proteins.faa'
+    cmd_a += ' --mode=proteins'
+    cmd_a += ' --lineage_dataset=' + row['busco_lineage'].values[0]
+    cmd_a += ' --cpu=' + str(args.threads)
+    cmd_a += ' --out=/data/species/' + str(args.species) + '/busco/annotfile'
+    cmd_a += ' &> ' + args.logfile
 
 # write command to output file
 try:
     with open(args.output, 'w') as f:
-        if len(cmd) > 0:
-            f.write(cmd)
+        f.write(cmd_g)
+        f.write('\n')
+        if(len(cmd_a)>0):
+            f.write(cmd_a)
             f.write('\n')
-        else:
-            f.write('echo "No annotation command needed for species ' + str(args.species)+ '" &> ' + args.logfile)
+        if(len(cmd_b)>0):
+            f.write(cmd_b)
+            f.write('\n')
 
 except IOError:
     print("Error: can't write to file" + args.output)
--- scripts/run_busco.sh
+++ scripts/run_busco.sh
@@ -21,6 +21,6 @@ conda activate busco_env
 cmd_file=data/checkpoints_annotate/${spid}_busco.cmd
 log_file=data/checkpoints_annotate/${spid}_busco.log
 wdir=data/species/${spid}/busco
-python3 scripts/build_busco_cmd.py -c ${csv} -s ${spid} -t ${threads} -o $cmd_file -b data/species/${spid}/braker/braker.aa -l $log_file -w $wdir
+python3 scripts/build_busco_cmd.py -c ${csv} -s ${spid} -t ${threads} -o $cmd_file  -l $log_file -w $wdir
 bash $cmd_file 
 touch ${output}
